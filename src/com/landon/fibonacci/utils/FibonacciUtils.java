90a868ee2e3e56a3cd5893d06961fc2c83618426
ktjjang01 <ktjjang01@gmail.com>
2014-01-12 20:20:08 -0800

    minor update

--- src/com/landon/fibonacci/utils/FibonacciUtils.java
+++ src/com/landon/fibonacci/utils/FibonacciUtils.java
@@ -1,7 +1,6 @@
 package com.landon.fibonacci.utils;
 
 public class FibonacciUtils {
-    
     // Prints array in a format [element 1, element 2, ...]
     public static void printArray(int[] list) {
         System.out.print("[");
