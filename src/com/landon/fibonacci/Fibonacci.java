ed7fa838ad5853821a6d225e1808cc7c65b7ca71
ktjjang01 <ktjjang01@gmail.com>
2014-02-17 21:20:11 -0800

    updated general structure

---
+++ src/com/landon/fibonacci/Fibonacci.java
@@ -0,0 +1,42 @@
+package com.landon.fibonacci;
+
+import com.landon.fibonacci.utils.FibonacciUtils;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        try {
+            if (args.length == 1) {
+                int num = Integer.parseInt(args[0]);
+
+                if (FibonacciUtils.isValidInt(num)) {
+                    FibonacciUtils.printArray(getFibonacciArray(num));
+                } else {
+                    System.out.println("Your input is either less than 0 or greater than 46. "
+                            + "Please pick a number between 1 to 46.");
+                }
+            } else {
+                System.out.println("Please enter single integer value.");
+            }
+        } catch (NumberFormatException nfe) { // Non-integer argument
+            System.out.println(nfe + "you can only pass integer as an argument.");
+        }
+    }
+
+    private static int[] getFibonacciArray(int n) {
+        int[] target = new int[n];
+
+        if (n == 0) return target;
+        if (n > 1) target[1] = 1;
+        if (n > 2) {
+            for (int i = 2; i <= n - 1; i++) {
+                target[i] = target[i - 2] + target[i - 1];
+            }
+        }
+        return target;
+    }
+
+    // More mathematically elegant, non-optimal solution
+    /*private static int recursiveFib(int n) {
+        return (n < 2) ? n : recursiveFib(n - 1) + recursiveFib(n - 2);
+    }*/
+}
--- src/com/landon/fibonacci/utils/FibonacciUtils.java
+++ src/com/landon/fibonacci/utils/FibonacciUtils.java
@@ -1,20 +1,25 @@
 package com.landon.fibonacci.utils;
 
 public class FibonacciUtils {
-    // Prints array in a format [element 1, element 2, ...]
     public static void printArray(int[] list) {
         System.out.print("[");
 
-        if (list.length == 0) {
+        int length = list.length;
+
+        if (length == 0) {
             System.out.print("");
-        } else if (list.length == 1) {
+        } else if (length == 1) {
             System.out.print(list[0]);
         } else {
-            for (int i = 0; i <= list.length - 2; i++) {
+            for (int i = 0; i <= length - 2; i++) {
                 System.out.print(list[i] + ", ");
             }
-            System.out.print(list[list.length - 1]);
+            System.out.print(list[length - 1]);
         }
         System.out.println("]");
+    }
+
+    public static boolean isValidInt(int n) {
+        return (n > 0 && n < 47);
     }
 }
