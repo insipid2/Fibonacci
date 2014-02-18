dfb31b29634416e891455159986fbe18809e05c2
ktjjang01 <ktjjang01@gmail.com>
2014-02-17 22:57:21 -0800

    testing

--- src/com/landon/fibonacci/Fibonacci.java
+++ src/com/landon/fibonacci/Fibonacci.java
@@ -22,7 +22,7 @@ public class Fibonacci {
         }
     }
 
-    private static int[] getFibonacciArray(int n) {
+    private static int[] getFibonacciArray(int n) { // TEST DUR?
         int[] target = new int[n];
 
         if (n == 0) return target;
